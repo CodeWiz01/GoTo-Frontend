b96165f395fe8939a61743895a2009fb4120595d
mphomolefe730 <mphomolefe730@gmail.com>
2024-01-18 03:04:47 +0200

    add backend link

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	googleApiKey:"AIzaSyCVYEXJ6qQ5rTgtL7WAAV3D2c1z8ZQQYws&q",
-	backendLink:""
+	backendLink:"http://goto-database.cd2qo02c4a7i.af-south-1.rds.amazonaws.com/"
 }
